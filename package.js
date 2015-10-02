286e009dcf7c06a44a8262b9d012397dfd5e49f4
Riza Fahmi <rizafahmi@gmail.com>
2015-10-02 19:26:36 +0700

    Protect polling submit. Only admin can do it

--- package.js
+++ package.js
@@ -13,6 +13,11 @@ Package.describe({
 Package.onUse(function(api) {
   api.use('templating', 'client');
   api.use(['iron:router@1.0.9'], 'client', {weak: false, unordered: false});
+  api.use([
+      
+      'aldeed:autoform@5.4.1',
+      'aldeed:collection2@2.3.3',
+  ]);
   api.versionsFrom('1.1.0.3');
 
   api.addFiles('telescope-polling.js');
