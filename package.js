b157163e4415d8f4f385a1a908c77475efc94702
Riza Fahmi <rizafahmi@gmail.com>
2015-10-07 13:54:09 +0700

    Add body to fixtures

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'riza:telescope-polling',
-  version: '0.1.5',
+  version: '0.1.6',
   // Brief, one-line summary of the package.
   summary: 'Telescope Polling Package',
   // URL to the Git repository containing the source code for this package.
